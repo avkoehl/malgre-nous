763715b905d129c9e56039f32f260e009fcb8ae7
avkoehl <avkoehl@ucdavis.edu>
2018-01-21 17:27:46 -0800

    visualizations on Overview.html updated to be d3 instead if just an image. Also, fixed the 'go back <' that was not appearing on the timeline

---
+++ Overview/casualties.js
@@ -0,0 +1,202 @@
+// Casualties.js
+// Version 1 11/25/2017
+//
+// Script that draws the war casualties from the Malgre Nous and German armies
+// It is designed for d3 v4
+//
+
+//
+// Get name of the Json file from HTML attribute given to the script
+//
+var scripts = document.getElementsByTagName ('script');
+for (var s, i = scripts.length; i && (s = scripts[--i]);) {
+        if ((t = s.getAttribute ('src')) && (t = t.match (/^(.*)casualties.js(\?\s*(.+))?\s*/))) {
+                jsonfile=s.getAttribute('jsonfile');
+        }
+}
+
+//
+// Get size of the window from the size of the HTML element and adapt for
+// margins
+//
+
+var margin = {top: 20, right: 80, bottom: 40, left: 100};
+var width = parseInt(document.getElementById("casualties").style.width,10)-margin.right -margin.left;
+var height = parseInt(document.getElementById("casualties").style.height,10)-margin.top-margin.bottom;
+
+var parseDate = d3.timeParse("%m-%Y");
+
+//
+// set the ranges
+//
+
+var x = d3.scaleTime().range([0, width]);
+var y0 = d3.scaleLinear().range([height, 0]);
+var y1 = d3.scaleLinear().range([height, 0]);
+var c0 = function(d) { return d.y0;};
+var c1 = function(d) { return d.y1;};
+
+//
+// append the svg obgect to the tag with the id "casualties" in the html page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+//
+
+var svg = d3.select("#casualties").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform",
+		"translate(" + margin.left + "," + margin.top + ")");
+
+//
+// Get the data from the Json file
+//
+
+d3.json(jsonfile, function(error, jdata) {
+	if (error) throw error;
+
+	var data = jdata.data;
+
+//
+// format the data
+//
+
+	data.forEach(function(d) {
+		d.date = parseDate(d.date);
+		d.y0 = +d.y0;
+		d.y1 = +d.y1;
+	});
+
+//
+// Scale the range of the data
+//
+
+	x.domain(d3.extent(data, function(d) { return d.date; }));
+	y0.domain([0, d3.max(data, function(d) { return d.y0; })]);
+	y1.domain([0, d3.max(data, function(d) { return d.y1; })]);
+
+//
+// Add the X Axis
+//
+
+	xAxis = d3.axisBottom(x);
+	y0Axis = d3.axisLeft(y0);
+	y1Axis = d3.axisRight(y1);
+
+	var axisX = svg.append("g")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis
+		.tickFormat(d3.timeFormat("%Y-%m")))
+		.style("font-size","14px")
+		.style("stroke", "black")
+		.style("stroke-width", 1.0);
+	axisX.selectAll("line").attr("fill","none");
+	axisX.selectAll("path").attr("fill","none");
+
+//
+// Add the Y0 Axis with text label
+//
+
+	var axisY0 = svg.append("g")
+		.call(y0Axis)
+		.style("font-size","14px")
+		.style("stroke", "red")
+		.style("stroke-width", 1.0);
+	axisY0.selectAll("line").attr("fill","none");
+	axisY0.selectAll("path").attr("fill","none");
+
+	svg.append("text")
+		.attr("class", "y label")
+		.attr("text-anchor", "end")
+		.style("font-size","20px")
+		.style("stroke", "red")
+		.attr("y", -86)
+		.attr("dy", ".75em")
+		.attr("transform", "rotate(-90)")
+		.text("German casualties");
+
+
+//
+// Add the Y1 Axis with text label
+//
+
+	var axisY1 = svg.append("g")
+		.attr("transform", "translate(" + width + " ,0)")
+		.call(y1Axis)
+		.style("font-size","14px")
+		.style("stroke", "blue")
+		.style("stroke-width", 1.0);
+	axisY1.selectAll("line").attr("fill","none");
+	axisY1.selectAll("path").attr("fill","none");
+
+	svg.append("text")
+		.attr("class", "y label")
+		.attr("text-anchor", "end")
+		.style("font-size","20px")
+		.style("stroke", "blue")
+		.attr("y", width+50)
+		.attr("dy", ".75em")
+		.attr("transform", "translate(" + width + " ,0)")
+		.attr("transform", "rotate(-90)")
+		.text("Malgre Nous casualties");
+
+//
+// Add box for the plot; fill in in weak yellow...
+//
+
+	svg.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("height", height)
+		.attr("width", width)
+		.style("stroke", "black")
+		.style("fill", "#FFFFF0")
+		.style("stroke-width", 0.5);
+
+//
+// Add the lines
+//
+	var valueline0 = d3.line()
+		.x(function(d) { return x(d.date); })
+		.y(function(d) { return y0(d.y0); });
+    
+	var valueline1 = d3.line()
+		.x(function(d) { return x(d.date); })
+		.y(function(d) { return y1(d.y1); });
+    
+	var line1=svg.append("path")  
+		.style("stroke", "red")
+		.style("stroke-width", "2")
+		.attr("fill", "none")
+		.attr("d", valueline0(data));
+
+	var line2=svg.append("path")  
+		.style("stroke", "blue")
+		.style("stroke-width", "2")
+		.attr("fill", "none")
+		.attr("d", valueline1(data));
+
+//
+// Add the scatterplot
+//
+
+	svg.selectAll(".dot0")
+		.data(data)
+		.enter().append("circle")
+		.attr("class", "dot")
+		.attr("r", 3.5)
+		.attr("cx", function(d) { return x(d.date); })
+		.attr("cy", function(d) { return y0(d.y0); })
+		.style("fill", "red");
+
+	svg.selectAll(".dot1")
+		.data(data)
+		.enter().append("circle")
+		.attr("class", "dot")
+		.attr("r", 3.5)
+		.attr("cx", function(d) { return x(d.date); })
+		.attr("cy", function(d) { return y1(d.y1); })
+		.style("fill", "blue");
+
+});
---
+++ Overview/regions.js
@@ -0,0 +1,180 @@
+// Regions.js
+// Version 1 11/25/2017
+//
+// Script that draws the war casualties from the Malgre Nous and German armies
+// on different fronts
+// It is designed for d3 v4
+//
+
+//
+// Get name of the Json file from HTML attribute given to the script
+//
+var scripts = document.getElementsByTagName ('script');
+for (var s, i = scripts.length; i && (s = scripts[--i]);) {
+        if ((t = s.getAttribute ('src')) && (t = t.match (/^(.*)regions.js(\?\s*(.+))?\s*/))) {
+                jsonfile=s.getAttribute('jsonfile');
+        }
+}
+
+//
+// Get size of the window from the size of the HTML element and adapt for
+// margins
+//
+
+var margin = {top: 20, right: 150, bottom: 40, left: 30};
+var width = parseInt(document.getElementById("casualties").style.width,10)-margin.right -margin.left;
+var height = parseInt(document.getElementById("casualties").style.height,10)-margin.top-margin.bottom;
+
+//
+// set the ranges
+//
+
+var x0 = d3.scaleBand()
+	.rangeRound([0,width])
+	.paddingInner(0.1);
+
+var x1 = d3.scaleBand()
+	.padding(0.05);
+
+var y = d3.scaleLinear()
+	.rangeRound([height, 0]);
+
+var color = d3.scaleOrdinal()
+	.range(["#ca0020","#0571b0","#f4a582","#d5d5d5","#92c5de","#0571b0"]);
+
+//
+// append the svg obgect to the tag with the id "casualties" in the html page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+//
+
+var svg2 = d3.select("#regions").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform",
+		"translate(" + margin.left + "," + margin.top + ")");
+
+//
+// Get the data from the Json file
+//
+
+d3.json(jsonfile, function(error, jdata) {
+	if (error) throw error;
+
+	var data = jdata;
+
+	var categoriesNames = data.map(function(d) { return d.categorie; });
+	var groupNames = data[0].values.map(function(d) { return d.name; });
+
+//
+// Scale the range of the data
+//
+
+	x0.domain(categoriesNames);
+	x1.domain(groupNames).rangeRound([0, x0.bandwidth()]);
+	y.domain([0, d3.max(data, function(categorie) { return d3.max(categorie.values, function(d) { return d.value; }); })]);
+
+//
+// Add the X Axis
+//
+
+	xAxis = d3.axisBottom(x0);
+	yAxis = d3.axisLeft(y);
+
+	var axisX = svg2.append("g")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis)
+		.style("font-size","14px")
+		.style("stroke", "black")
+		.style("stroke-width", 1.0);
+	axisX.selectAll("line").attr("fill","none");
+	axisX.selectAll("path").attr("fill","none");
+
+//
+// Add the Y Axis with text label
+//
+
+	var axisY = svg2.append("g")
+		.call(yAxis)
+		.style("font-size","14px")
+		.style("stroke", "black")
+		.style("stroke-width", 1.0);
+	axisY.selectAll("line").attr("fill","none");
+	axisY.selectAll("path").attr("fill","none");
+
+	svg2.append("text")
+		.attr("class", "y label")
+		.attr("text-anchor", "end")
+		.style("font-size","20px")
+		.style("stroke", "black")
+		.attr("y", -56)
+		.attr("dy", ".75em")
+		.attr("transform", "rotate(-90)")
+		.text("Casualties (%)");
+
+//
+// Add box for the plot; fill in in weak yellow...
+//
+
+	svg2.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("height", height)
+		.attr("width", width)
+		.style("stroke", "black")
+		.style("fill", "#FFFFF0")
+		.style("stroke-width", 0.5);
+
+//
+// Add the bar charts
+//
+	var slice = svg2.selectAll(".slice")
+		.data(data)
+		.enter().append("g")
+		.attr("class", "g")
+		.attr("transform",function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
+
+	slice.selectAll("rect")
+		.data(function(d) { return d.values; })
+		.enter().append("rect")
+		.attr("width", x1.bandwidth())
+		.attr("x", function(d) { return x1(d.name); })
+		.style("fill", function(d) { return color(d.name) })
+		.attr("y", function(d) { return y(d.value); })
+		.attr("height", function(d) { return height - y(d.value); })
+		.on("mouseover", function(d) {
+			d3.select(this).style("fill", d3.rgb(color(d.name)).darker(2));
+		})
+		.on("mouseout", function(d) {
+			d3.select(this).style("fill", color(d.name));
+		});
+
+//
+// Add legends
+//
+	var legend = svg2.selectAll(".legend")
+//		.data(data[0].values.map(function(d) { return d.name; }).reverse())
+		.data(groupNames)
+		.enter().append("g")
+		.attr("class", "legend")
+		.attr("transform", function(d,i) { return "translate(0," + i * 30 + ")"; })
+//		.style("opacity","0");
+		;
+
+	legend.append("rect")
+		.attr("x", width - 30)
+		.attr("width", 25)
+		.attr("height", 25)
+		.style("fill", function(d) { return color(d); });
+
+	legend.append("text")
+		.attr("x", width - 50)
+		.attr("y", 9)
+		.attr("dy", ".35em")
+		.style("font-size","16px")
+		.style("stroke", "black")
+		.style("text-anchor", "end")
+		.text(function(d) {return d; });
+
+});
